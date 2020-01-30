e033b8131a90db9b22716aac71fc09b9add38670
Marco Tisi <marco.tisi@gmail.com>
2020-01-30 12:25:41 +0000

    Add comment

--- src/dependency-service.provider.ts
+++ src/dependency-service.provider.ts
@@ -2,6 +2,7 @@ import { DependencyService } from "./dependency.service";
 
 export const DependencyServiceProvider = {
   provide: DependencyService,
+  // The same happens with useFactory
   // useFactory: () => {
   //   return new DependencyService();
   // },
